524fabf3b7728429d48f685ea1a1193bb51216b6
Your Name <you@example.com>
2018-12-22 11:27:58 +0800

    12月22号

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,20 @@ const order = require('./order');
 const login = require('./login');
 let Router = express.Router();
 
+//给所有路由设置允许跨域
+// Router.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
+//     // 跨域请求CORS中的预请求
+//     if(req.method=="OPTIONS") {
+//       res.send(200);/*让options请求快速返回*/
+//     } else{
+//       next();
+//     }
+// });
+
 // 关于用户的路由
 Router.use('/html/user',userRouter);
 
